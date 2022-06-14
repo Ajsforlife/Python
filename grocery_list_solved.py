9a675bf73380fe9e01709c6401b3e1e6a1c2eb1d
Andrew Stein <ajsforlife@icloud.com>
2022-06-14 17:03:45 -0600

    Add files via upload

---
+++ grocery_list_solved.py
@@ -0,0 +1,14 @@
+# Create a Python list to store your grocery list
+grocery_list = ["Peanut Butter","Jelly","Bread","FOOD"]
+
+# Print the grocery list
+print(grocery_list)
+
+# Change "Peanut Butter" to "Almond Butter" and print out the updated list
+grocery_list[0] = "Almond Butter"
+# Remove "Jelly" from grocery list and print out the updated list
+grocery_list.remove("Jelly")
+# Add "Coffee" to grocery list and print the updated list
+grocery_list.append("Coffee")
+
+print(grocery_list)
