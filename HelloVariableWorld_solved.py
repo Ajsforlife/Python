3e654fa65394c871bf63a05ac0e86bed8bde7e41
Andrew Stein <ajsforlife@icloud.com>
2022-06-14 17:03:56 -0600

    Add files via upload

---
+++ HelloVariableWorld_solved.py
@@ -0,0 +1,36 @@
+# Create a variable called 'name' that holds a string
+name = str(input("What is your name?"))
+
+# Create a variable called 'country' that holds a string
+country = str(input("Where are you from?"))
+
+# Create a variable called 'age' that holds an integer
+age = int(input("How old are you?"))
+
+# Create a variable called 'hourly_wage' that holds an integer
+hourly_wage = int(input("How much do you make per hour?"))
+
+# Calculate the daily wage for the user
+daily_wage = hourly_wage * 8
+
+# Create a variable called 'satisfied' that holds a boolean
+satisfied = 
+
+# Print out "Hello <name>!"
+print("Hello " + name)
+
+# Print out what country the user entered
+print(country)
+
+# Print out the user's age
+print(age)
+
+# With an f-string, print out the daily wage that was calculated
+print(f"{name} makes {daily_wage}$ per day.")
+
+# With an f-string, print out whether the users were satisfied
+    if satisfied = True
+    print(f"{name} was satisfied.")
+    else
+    print(f"{name} wasn't satisfied.")
+
