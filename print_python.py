c935a97cfa4a06d13bebfa2ad5e8ba3daffaa9ba
Andrew Stein <ajsforlife@icloud.com>
2022-06-14 12:01:15 -0600

    Add files via upload

--- print_python.py
+++ print_python.py
@@ -1,4 +1,21 @@
-my_votes = int(input("How many votes did you get in the election? "))
-total_votes = int(input("What is the total votes in the election? "))
+#practice 1
+#my_votes = int(input("How many votes did you get in the election? "))
+#total_votes = int(input("What is the total votes in the election? "))
 #percentage_votes = (my_votes / total_votes) * 100
-print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+#print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+
+# practice2
+#counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+#for county, voters in counties_dict.items():
+    #print(county + "county has" + str(voters) + "registered voters.")
+    #print(f"{county} county has {voters} registered voters.")
+
+#practice 3
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+
+print(message_to_candidate)
