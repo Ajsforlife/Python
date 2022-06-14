7c913eb39a51108bda74b0180c4deadd4dd1de4d
Andrew Stein <ajsforlife@icloud.com>
2022-06-14 14:22:51 -0600

    Add files via upload

--- voting_data.py
+++ voting_data.py
@@ -1,10 +1,16 @@
-voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
-              {"county":"Denver", "registered_voters":463353},
-              {"county":"Jefferson", "registered_voters": 432438}]
-#for county_dict in voting_data:
+voting_data = [{'county':'Arapahoe', 'registered_voters': 422829},
+              {'county':'Denver', 'registered_voters':463353},
+              {'county':'Jefferson', 'registered_voters': 432438}]
+#for county_dict in voting_data
     #print(county_dict)
 #for i in range(len(voting_data)):
     #print(voting_data[i]['county'])
-for county_dict in voting_data:
-    for value in county_dict.values():
-        print(value)
+#county and voters output
+#for county_dict in voting_data:
+#   for value in county_dict.values():
+#        print(value)
+#just voters output
+#for county_dict in voting_data:
+ #   print(county_dict['registered_voters'])
+for i in range(len(voting_data)):
+    print(f"{voting_data[i]['county']} county has {voting_data[i]['registered_voters']:,} registered voters.")
